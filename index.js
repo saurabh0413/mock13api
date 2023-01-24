61a578f11f1422c7c6222eafc4cf336589767154
saurabh <saurabhfarkade83@gmail.com>
2023-01-24 12:00:01 +0530

    folder-added

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { connection } = require("./config/db");
+const cors = require('cors');
+const { userRoute } = require("./routes/user.routes");
+const { authentication } = require("./middlewares/authentication");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("welcome to home page");
+});
+
+app.use("/user", userRoute);
+app.use(authentication);
+app.use("/profile",userRoute)
+app.listen(8787, async() => {
+    try {
+        await connection;
+        console.log("connection established");
+      } catch (err) {
+        console.log(err);
+      }
+      console.log("server started on port 8787");
+});
